5ac6bd569e72a0ef25dee1d6f1695b437df1fdd0
antonio.conceicao <antonio.conceicao@BRPRDKT002853.valueteam.com.br>
2017-03-10 13:20:29 -0300

    Implementado o metodo de delete

--- gestaoFesta/src/main/java/com/br/festa/controller/ConcidadosController.java
+++ gestaoFesta/src/main/java/com/br/festa/controller/ConcidadosController.java
@@ -1,9 +1,19 @@
 package com.br.festa.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.br.festa.model.Convidado;
@@ -26,7 +36,27 @@ public class ConcidadosController {
 
 	@PostMapping("/convidados")
 	public String salvar(Convidado convidado){
+		
 		this.convidados.save(convidado);
+		return "redirect:/convidados";
+	}
+	
+	
+	
+	/****
+	 * o thymeleaf não suporta metodos HTTP DELETE e PUT. somente POST e GET
+	 * 
+	 * */ 
+	
+	@GetMapping(value="/convidados/{id}")
+	public String deletar(@RequestParam ("id") String id){
+	
+		
+			
+		 Long idConvert = Long.parseLong(id);
+		
+		Convidado convidadoEncontrado= convidados.findOne(idConvert);
+	    this.convidados.delete(convidadoEncontrado);	
 		return "redirect:/convidados";
 	}
 }
